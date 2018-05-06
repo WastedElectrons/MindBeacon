320e584c11b880ee543dce49cd3604e13dfbb33f
WastedElectrons <WastedElectrons@outlook.com>
2018-05-06 19:00:08 -0400

    Added image skeleton
    Added an ImageController with a Get method (not implemented)
    Added an Image model with an ImageRepo interface.

---
+++ MindBeacon/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MindBeacon.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Image")]
+    public class ImageController : Controller
+    {
+        // GET: api/Image
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MindBeacon/Models/Image.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindBeacon.Models
+{
+    /// <summary>
+    /// Describes a repository to manage Images.
+    /// </summary>
+    public interface ImageRepo
+    {
+        /// <summary>
+        /// Fetches all images.
+        /// </summary>
+        /// <returns>A list of all images</returns>
+        List<Image> GetAll();
+
+        /// <summary>
+        /// Adds an Image, or updates an Image if it exists in the repo.
+        /// </summary>
+        /// <param name="image">The image to Add or Update</param>
+        void AddOrUpdate(Image image);
+
+        /// <summary>
+        /// Removes an image from the repository if it exists.
+        /// </summary>
+        /// <param name="id">The Id of the image to remove.</param>
+        void Delete(int id);
+    }
+
+    /// <summary>
+    /// A galary image model.
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        /// The Id of the Image.  This is a FK to the origional metadata.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The time the Image was uploaded.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// The name of the Image.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The URL the image can be found at.
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// A repository to manage Image persistance.
+        /// </summary>
+        public static ImageRepo Repo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
