66ce40241432ff43c771f338377ca6a0af5dc8c5
WastedElectrons <WastedElectrons@outlook.com>
2018-05-06 19:46:47 -0400

    Added an in memory cache image repo.
    Repo stores all images in process memory.

---
+++ MindBeacon.Tests/Unit Tests/InMemoryCacheImageRepoTests.cs
@@ -0,0 +1,128 @@
+﻿using MindBeacon.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MindBeacon.Tests.Unit_Tests
+{
+    [TestFixture]
+    public class InMemoryCacheImageRepoTests
+    {
+        [Test]
+        public void AddOrUpdate_ImageDoesNotExist_AddsImage()
+        {
+            var repo = GetRepo();
+
+            repo.AddOrUpdate(Image1);
+
+            AssertImagesEqual(Image1, repo.GetAll().Single());
+        }
+
+        [Test]
+        public void AddOrUpdate_ImageExists_UpdatesImage()
+        {
+            var repo = GetRepo();
+
+            repo.AddOrUpdate(Image1);
+
+            var newImage = Image1;
+            newImage.Name = "new Name";
+            repo.AddOrUpdate(newImage);
+
+            AssertImagesEqual(newImage, repo.GetAll().Single());
+        }
+
+        [Test]
+        public void AddOrUpdate_WithOtherImages_AddsImage()
+        {
+            var repo = GetRepo();
+
+            repo.AddOrUpdate(Image2);
+            repo.AddOrUpdate(Image1);
+
+            AssertImagesEqual(Image1, repo.GetAll().Single(i => i.Id == Image1.Id));
+        }
+
+        [Test]
+        public void AddOrUpdate_WithExistingAndOtherImages_UpdatesImage()
+        {
+            var repo = GetRepo();
+
+            repo.AddOrUpdate(Image2);
+            repo.AddOrUpdate(Image1);
+
+            var newImage = Image1;
+            newImage.Name = "new Name";
+            repo.AddOrUpdate(newImage);
+
+            AssertImagesEqual(newImage, repo.GetAll().Single(i => i.Id == Image1.Id));
+        }
+
+        [Test]
+        public void Delete_ImageExists_RemovesImage()
+        {
+            var repo = GetRepo();
+            repo.AddOrUpdate(Image1);
+            repo.Delete(Image1.Id);
+            CollectionAssert.IsEmpty(repo.GetAll());
+        }
+
+        [Test]
+        public void Delete_ImageDoesNotExist_DoesNothing()
+        {
+            var repo = GetRepo();
+            repo.AddOrUpdate(Image2);
+            Assert.DoesNotThrow(() => repo.Delete(1));
+            Assert.AreEqual(1, repo.GetAll().Count());
+        }
+
+        [Test]
+        public void GetAll_RepoEmpty_ReturnsEmptyList()
+        {
+            var repo = GetRepo();
+            Assert.AreEqual(0, repo.GetAll().Count());
+        }
+
+        [Test]
+        public void GetAll_RepoNotEmpty_ReturnsRepoContents()
+        {
+            var repo = GetRepo();
+            repo.AddOrUpdate(Image1);
+            AssertImagesEqual(Image1, repo.GetAll().Single());
+        }
+
+        private void AssertImagesEqual(Image expected, Image actual)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected.Id, actual.Id);
+                Assert.AreEqual(expected.Name, actual.Name);
+                Assert.AreEqual(expected.CreatedAt, actual.CreatedAt);
+                Assert.AreEqual(expected.ImageUrl, actual.ImageUrl);
+            });
+        }
+
+        private Image Image1 => new Image()
+        {
+            Id = 1,
+            Name = "Image 1",
+            CreatedAt = new DateTime(11222333),
+            ImageUrl = "image.com/1"
+        };
+
+        private Image Image2 => new Image()
+        {
+            Id = 2,
+            Name = "Image 2",
+            CreatedAt = new DateTime(11222443),
+            ImageUrl = "image.com/2"
+        };
+
+        private ImageRepo GetRepo()
+        {
+            return new Models.ImageRepoImpl.InMemoryCacheImageRepo();
+        }
+    }
+}
---
+++ MindBeacon/Models/ImageRepo/InMemoryCacheImageRepo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindBeacon.Models.ImageRepoImpl
+{
+    /// <summary>
+    /// A repository that stores Images in an in memory cache.
+    /// </summary>
+    /// <remarks>
+    ///     This repository has two obvious limitations:
+    ///     
+    ///     First, as all there is an obvious risk of an out of memory
+    ///     exception as the number of images grows.
+    ///     
+    ///     Second, this cache is single instance.  Multiple instances
+    ///     of this service will not be able to share the cache. This
+    ///     will lead to excessive calls to the external service.
+    ///     
+    ///     This will work for demonstration purposes. Howerver, this
+    ///     should be replaced with a repo pointing to a caching service
+    ///     later.
+    /// </remarks>
+    public class InMemoryCacheImageRepo : ImageRepo
+    {
+        private readonly IDictionary<int, Image> _cache
+            = new Dictionary<int, Image>();
+
+        public void AddOrUpdate(Image image)
+        {
+            if (ImageExists(image.Id))
+            {
+                _cache[image.Id] = image;
+            }
+            else
+            {
+                _cache.Add(image.Id, image);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (ImageExists(id))
+            {
+                _cache.Remove(id);
+            }
+        }
+
+        public List<Image> GetAll()
+        {
+            return _cache.Values.ToList();
+        }
+
+        private bool ImageExists(int id)
+            => _cache.ContainsKey(id);
+    }
+}
